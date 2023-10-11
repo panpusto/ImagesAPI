3bb6f6fb0c9ba5e85532fc91041cc1f4a4fa0293
Cezary <102999499+panpusto@users.noreply.github.com>
2023-10-11 11:31:02 +0200

    #7-feature:add-response-message-after-upload-img

--- images/views.py
+++ images/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .serializers import ImageCreateSerializer, ImageListSerializer
 from .models import Image
 
@@ -14,4 +15,16 @@ class ImageListCreateAPIVIew(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
-        
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+
+            success_message = "Image uploaded successfully."
+            response_data = {
+                 "message": success_message
+            }
+
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
